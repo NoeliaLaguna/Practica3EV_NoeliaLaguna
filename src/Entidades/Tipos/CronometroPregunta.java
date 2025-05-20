3358de5b1f4008f6c6266a057093b924535b15f4
NoeliaLaguna <noelialaguna94@gmail.com>
2025-05-20 18:57:49 +0200

    Borrador Inicial del programa

--- src/Entidades/Tipos/CronometroPregunta.java
+++ src/Entidades/Tipos/CronometroPregunta.java
@@ -1,5 +1,10 @@
 package Entidades.Tipos;
 
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Clase para implementar la interfaz Entidades.Tipos.Pregunta.
  * Se implementan los metodos preguntar y evaluar respuesta.
@@ -8,6 +13,10 @@ package Entidades.Tipos;
  * @version 1.0
  */
 public class CronometroPregunta implements Pregunta {
+
+    int segundos;
+    LocalTime inicio;
+    LocalTime fin;
 
     @Override
     public int getNumeroIntentos() {
@@ -19,9 +28,9 @@ public class CronometroPregunta implements Pregunta {
      */
     @Override
     public void preguntar() {
-
-        //Preguntar al profe
-
+        Random segundosRndm = new Random();
+        this.segundos = segundosRndm.nextInt(1, 6);
+        System.out.printf("Debes pulsar ENTER una vez. Una vez pulses ENTER, tienes que esperar %d segundos y pulsar ENTER de nuevo. \n" + " Yo contaré el tiempo y si aciertas te doy los puntos.\n", segundos);
     }
 
     /**
@@ -32,6 +41,22 @@ public class CronometroPregunta implements Pregunta {
      */
     @Override
     public boolean evaluarRespuesta(String respuesta) {
+        Scanner teclado = new Scanner(System.in);
+        teclado.nextLine();
+        inicio = LocalTime.now();
+        System.out.println("Inicio");
+
+        teclado.nextLine();
+        fin = LocalTime.now();
+
+        long segundosPulsacion = Duration.between(inicio, fin).toSeconds();
+        long milisegundos = Duration.between(inicio, fin).toMillis();
+        milisegundos = milisegundos - segundosPulsacion * 1000;
+
+        System.out.printf("Has pulsado en %d, %d segundos\n", segundosPulsacion, milisegundos);
+
+        //TODO: EVALUAR EL TIEMPO CON EL MARGEN DE ERROR (± 0.5 segundos).
+
         return false;
     }
 
